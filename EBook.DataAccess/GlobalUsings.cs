d0627a76e6b4c65ff12e237ee1a77d577c427795
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-07 16:28:52 +0200

    Moved used namespace from location to another

--- EBook.DataAccess/GlobalUsings.cs
+++ EBook.DataAccess/GlobalUsings.cs
@@ -2,4 +2,3 @@
 global using System.Linq.Expressions;
 global using EBook.DataAccess.Interfaces;
 global using Microsoft.EntityFrameworkCore;
-global using System.ComponentModel.DataAnnotations.Schema;
