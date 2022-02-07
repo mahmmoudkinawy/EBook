940d267b0aafae7fb03e2bcc6390a1d3df5949a3
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-07 12:17:57 +0200

    Added create cover type

---
+++ EBook.Web/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,28 @@
+﻿namespace EBook.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class CoverTypeController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CoverTypeController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+    public IActionResult Index() => View(_unitOfWork.CoverTypeRepository.GetAll());
+
+    public IActionResult Create() => View();
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(CoverType coverType)
+    {
+        if (ModelState.IsValid)
+        {
+            _unitOfWork.CoverTypeRepository.Add(coverType);
+            _unitOfWork.Save();
+            TempData["success"] = "Cover Type Created Successfully";
+            return RedirectToAction("Index");
+        }
+        return View(coverType);
+    }
+}
+
