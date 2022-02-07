3300fbbab56ab199439cd251a0e773cb675b8688
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-07 16:58:35 +0200

    Added Upsert end point and make it's logic

--- EBook.Web/Areas/Admin/Controllers/ProductController.cs
+++ EBook.Web/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿namespace EBook.Web.Areas.Admin.Controllers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EBook.Web.Areas.Admin.Controllers;
 public class ProductController : BaseAdminController
 {
     private readonly IUnitOfWork _unitOfWork;
@@ -6,4 +8,28 @@ public class ProductController : BaseAdminController
     public ProductController(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
 
     public IActionResult Index() => View(_unitOfWork.ProductRepository.GetAll());
+
+    public IActionResult Upsert(int? id)
+    {
+        var categoryList = _unitOfWork.CategoryRepository.GetAll()
+                    .Select(c => new SelectListItem
+                    {
+                        Text = c.Name,
+                        Value = c.Id.ToString()
+                    });
+
+
+        if (id == null || id == 0)
+        {
+            //Create new Product Model
+            ViewBag.categoryList = categoryList;
+            return View(new Product());
+        }
+        else
+        {
+
+        }
+
+        return View();
+    }
 }
