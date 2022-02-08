e50602bb41655d7e98ebc4443222acb46be13c31
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-08 17:07:49 +0200

    Added data anotations

--- EBook.Models/Product.cs
+++ EBook.Models/Product.cs
@@ -17,24 +17,30 @@ public class Product
 
     [Required]
     [Range(1, 10000)]
+    [Display(Name = "List Price")]
     public double ListPrice { get; set; }
 
     [Required]
     [Range(1, 10000)]
+    [Display(Name = "Price for 1-50")]
     public double Price { get; set; }
 
     [Required]
     [Range(1, 10000)]
+    [Display(Name = "Price for 51-100")]
     public double Price50 { get; set; }
 
     [Required]
     [Range(1, 10000)]
+    [Display(Name = "Price for 100+")]
     public double Price100 { get; set; }
 
     [ValidateNever]
+    [Display(Name = "Image")]
     public string ImageUrl { get; set; }
 
     [Required]
+    [Display(Name = "Category")]
     public int CategoryId { get; set; }
 
     [ForeignKey("CategoryId")]
@@ -42,6 +48,7 @@ public class Product
     public Category Category { get; set; }
 
     [Required]
+    [Display(Name = "Cover Type")]
     public int CoverTypeId { get; set; }
 
     [ForeignKey("CoverTypeId")]
