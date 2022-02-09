42a2c878dd36b0defafd6be5760ca4149aad1282
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-09 12:40:28 +0200

    Added Details end point and it's views

---
+++ EBook.Models/ShoppingCart.cs
@@ -0,0 +1,8 @@
+﻿namespace EBook.Models;
+public class ShoppingCart
+{
+    public Product Product { get; set; }
+
+    [Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
+    public int Count { get; set; }
+}
--- EBook.Web/Areas/Customer/Controllers/HomeController.cs
+++ EBook.Web/Areas/Customer/Controllers/HomeController.cs
@@ -7,5 +7,17 @@ public class HomeController : BaseCustomerController
 
     public IActionResult Index()
         => View(_unitOfWork.ProductRepository.GetAll(includeProperties: "Category,CoverType"));
+
+    public IActionResult Details(int id)
+    {
+        var shoppingCart = new ShoppingCart
+        {
+            Product = _unitOfWork.ProductRepository.GetFirstOrDefault(p => p.Id == id,
+            includeProperties: "Category,CoverType"),
+            Count = 1
+        };
+
+        return View(shoppingCart);
+    }
 }
 
