1ccf5bc8dc74c2c8a29b1c98e968ae76b7c1f1c2
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-13 17:38:30 +0200

    Added summary page and increment and decreament and remove from cart

--- EBook.Web/Areas/Customer/Controllers/CartController.cs
+++ EBook.Web/Areas/Customer/Controllers/CartController.cs
@@ -25,6 +25,40 @@ public class CartController : BaseCustomerController
         return View(shoppingCart);
     }
 
+    public IActionResult Summary() => View();
+
+    public IActionResult Plus(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCartRepository.GetFirstOrDefault(c => c.Id == cartId);
+        _unitOfWork.ShoppingCartRepository.IncrementCount(cart, 1);
+        _unitOfWork.Save();
+        //TempData["success"] = "Incremented Successfully";
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Minus(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCartRepository.GetFirstOrDefault(c => c.Id == cartId);
+
+        if (cart.Count <= 1)
+            _unitOfWork.ShoppingCartRepository.Remove(cart);
+
+        _unitOfWork.ShoppingCartRepository.DecrementCount(cart, 1);
+
+        _unitOfWork.Save();
+        //TempData["success"] = "Incremented Successfully";
+        return RedirectToAction(nameof(Index));
+    }
+
+    public IActionResult Remove(int cartId)
+    {
+        var cart = _unitOfWork.ShoppingCartRepository.GetFirstOrDefault(c => c.Id == cartId);
+        _unitOfWork.ShoppingCartRepository.Remove(cart);
+        _unitOfWork.Save();
+        //TempData["success"] = "Incremented Successfully";
+        return RedirectToAction(nameof(Index));
+    }
+
     private static double GetPriceBasedOnQuantity(int quantity, double price, double price50, double price100)
     {
         if (quantity <= 50)
