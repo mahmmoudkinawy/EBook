973a6d09ee41f76d159a2e8809ee7916192f4d67
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-16 20:00:13 +0200

    Added view component for solving session bug

---
+++ EBook.Web/ViewCompnents/ShoppingCartViewComponent.cs
@@ -0,0 +1,33 @@
+﻿namespace EBook.Web.ViewCompnents;
+public class ShoppingCartViewComponent : ViewComponent
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ShoppingCartViewComponent(IUnitOfWork unitOfWork) => _unitOfWork = unitOfWork;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var claimsIdentity = (ClaimsIdentity)User.Identity;
+        var user = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if (user != null)
+        {
+            if (HttpContext.Session.GetInt32(Constants.SessionCart) != null)
+            {
+                return View(HttpContext.Session.GetInt32(Constants.SessionCart));
+            }
+            else
+            {
+                HttpContext.Session.SetInt32(Constants.SessionCart,
+                    _unitOfWork.ShoppingCartRepository.GetAll(u => u.AppUserId == user).ToList().Count);
+                return View(HttpContext.Session.GetInt32(Constants.SessionCart));
+            }
+        }
+        else
+        {
+            HttpContext.Session.Clear();
+            return View(0);
+        }
+    }
+}
+
