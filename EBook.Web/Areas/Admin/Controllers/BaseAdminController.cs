8d956c04998d8c3bab413aa76d2e1e08cad5816f
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-16 17:04:35 +0200

    Configured authorization successfully

--- EBook.Web/Areas/Admin/Controllers/BaseAdminController.cs
+++ EBook.Web/Areas/Admin/Controllers/BaseAdminController.cs
@@ -1,6 +1,7 @@
 ﻿namespace EBook.Web.Areas.Admin.Controllers;
 
 [Area("Admin")]
+[Authorize(Roles = Constants.RoleAdmin + "," + Constants.RoleEmployee)]
 public abstract class BaseAdminController : Controller
 {
 }
