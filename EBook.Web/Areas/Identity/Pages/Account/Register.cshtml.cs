32321a44dc4e5c29975a1757f4574f54cd3c4f60
mahmmoudkinawy <mahmmoudkinawy@gmail.com>
2022-02-17 19:19:17 +0200

    Hide roles and make it just for the user

--- EBook.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ EBook.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -200,7 +200,11 @@ namespace EBook.Web.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(Constants.RoleAdmin))
+                            TempData["success"] = "New User Created Successfully";
+                        else
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+
                         return LocalRedirect(returnUrl);
                     }
                 }
